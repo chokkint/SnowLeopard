fac51cc89142579c94ff6cb87ce7e49d84310e5c
LTWangxi <>
2019-04-02 22:13:11 +0800

    添加客户动态画面

--- app/src/main/java/com/org/snowleopard/MainActivity.java
+++ app/src/main/java/com/org/snowleopard/MainActivity.java
@@ -1,13 +1,44 @@
 package com.org.snowleopard;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_customer:
+
+                    intent.setClass(MainActivity.this, CustomerHandPickActivity.class);
+                    return true;
+                case R.id.navigation_dynamic:
+
+                    return true;
+                case R.id.navigation_mine:
+
+                    return true;
+            }
+            startActivity(intent);
+            return false;
+        }
+    };
+
 }
